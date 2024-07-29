82fb542ee86a6f11b78ac33b247413b6fe24fd01
Dobee-Kim <dorosy555@naver.com>
2024-07-30 08:54:14 +0900

    0730

--- BOJ_10844_쉬운계단수.py
+++ BOJ_10844_쉬운계단수.py
@@ -4,9 +4,9 @@ dp = [[0]*10 for _ in range(0,N+1)]
 
 # print(dp)
 tmp = [0]
-tmp.extend([1]*8)
+tmp.extend([1]*9)
 dp[1] = tmp
-print(tmp)
+# print(tmp)
 
 for n in range(2, N+1):
     for k in range(0,10):
@@ -16,6 +16,7 @@ for n in range(2, N+1):
             dp[n][k] = dp[n-1][k-1]
         else:
             dp[n][k] = dp[n-1][k-1] + dp[n-1][k+1]
-        print(dp)
+        # print(dp)
 
-print(sum(dp))
+res = sum(dp[N])
+print(res % 1000000000)
